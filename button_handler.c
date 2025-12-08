0122033891e32038132a6052cc840915df50709e
moomin <tosh23fz@student.ju.se>
2025-12-08 15:25:53 +0200

    fixed the lora length problem

--- button_handler.c
+++ button_handler.c
@@ -5,41 +5,49 @@
 
 
 void wait_calib_button_handler(Dispenser* dis) {
-    static uint64_t last_blink = 0;
-    static bool led_state = false;
 
-    uint64_t now = time_us_64();
-    if (now - last_blink > LED_BLINK_US) {
-        led_state = !led_state;
-        gpio_put(dis->led_pin, led_state);
-        last_blink = now;
-    }
-    if (gpio_get(dis->button_pin) == 0) {
-        printf("Button pressed. Start calibration...\n");
+    while (dis->state == ST_WAIT_CALIBRATION) {
+        static uint64_t last_blink = 0;
+        static bool led_state = false;
 
-        gpio_put(dis->led_pin, 0);
-        dis->state = ST_CALIBRATION;
+        uint64_t now = time_us_64();
+        if (now - last_blink > LED_BLINK_US) {
+            led_state = !led_state;
+            gpio_put(dis->led_pin, led_state);
+            last_blink = now;
+        }
+        if (gpio_get(dis->button_pin) == 0) {
+            printf("Button pressed. Start calibration...\n");
 
-        while (gpio_get(dis->button_pin) == 0) {
-            sleep_ms(BUTTON_DEBOUNCE_MS);
+            gpio_put(dis->led_pin, 0);
+            dis->state = ST_CALIBRATION;
+
+            while (gpio_get(dis->button_pin) == 0) {
+                sleep_ms(BUTTON_DEBOUNCE_MS);
+            }
         }
     }
 }
 
 void wait_dispensing_button_handler(Dispenser* dis) {
-    gpio_put(dis->led_pin, 1);
 
-    if (gpio_get(dis->button_pin2) == 0) {
-        printf("Button pressed. Start dispensing...\n");
+    while (dis->state == ST_WAIT_DISPENSING) {
+        gpio_put(dis->led_pin, 1);
 
-        gpio_put(dis->led_pin, 0);
-        dis->state = ST_DISPENSING;
+        if (gpio_get(dis->button_pin2) == 0) {
+            printf("Button pressed. Start dispensing...\n");
+            dis->next_dispense_time = make_timeout_time_ms(dis->interval_ms);
+            dis->state = ST_DISPENSING;
+            printf("[FSM] START pressed -> enter ST_DISPENSING, first after %u ms\n",
+                   dis->interval_ms);
+            gpio_put(dis->led_pin, 0);
+            dis->state = ST_DISPENSING;
 
-        while (gpio_get(dis->button_pin2) == 0) {
-            sleep_ms(BUTTON_DEBOUNCE_MS);
+            while (gpio_get(dis->button_pin2) == 0) {
+                sleep_ms(BUTTON_DEBOUNCE_MS);
+            }
         }
     }
-
 }
 
 void led_blink(Dispenser*dis,int time) {
